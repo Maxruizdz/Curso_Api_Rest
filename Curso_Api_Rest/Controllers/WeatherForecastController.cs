091658336b68b8fb48474724be1771331394cc88
Maxi Ruiz Diaz <maxruizdz@hotmail.com>
2023-02-24 07:28:21 -0300

    Atributos para verbos HTTP

--- Curso_Api_Rest/Controllers/WeatherForecastController.cs
+++ Curso_Api_Rest/Controllers/WeatherForecastController.cs
@@ -10,23 +10,56 @@ public class WeatherForecastController : ControllerBase
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
+    private static List<WeatherForecast> listweatherForecasts = new List<WeatherForecast>();
+
 
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+        if (listweatherForecasts == null || !listweatherForecasts.Any())
+        {
+            listweatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return listweatherForecasts;
+    
     }
+    [HttpPost]
+
+    public IActionResult Post(WeatherForecast weatherForecast) {
+
+
+        listweatherForecasts.Add(weatherForecast);
+
+
+        return Ok(); 
+    
+    
+    }
+    [HttpDelete("{index}")]
+
+
+    public IActionResult Delete(int index) {
+        try
+        {
+            listweatherForecasts.RemoveAt(index);
+        }
+        catch (ArgumentOutOfRangeException) {
+            return BadRequest();
+        
+        }
+        return Ok();
+    }
+   
 }
