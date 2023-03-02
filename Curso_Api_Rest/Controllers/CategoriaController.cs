98f7f8bc74858319fe030ae8bc91443b34e8462b
Maxi Ruiz Diaz <maxruizdz@hotmail.com>
2023-03-02 19:34:42 -0300

    Creacion de clase controller de tarea y categoria

---
+++ Curso_Api_Rest/Controllers/CategoriaController.cs
@@ -0,0 +1,62 @@
+﻿using Curso_Api_Rest.Services;
+using Microsoft.AspNetCore.Mvc;
+using webapi.Modelos;
+
+namespace Curso_Api_Rest.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriaController : ControllerBase
+    {
+        ICategoriaService categoria_services;
+
+
+
+
+        public CategoriaController(ICategoriaService categoria) {
+
+            categoria_services = categoria;
+
+
+        }
+
+        [HttpGet]
+        public IActionResult Get() {
+
+            return Ok(categoria_services.Get());
+
+
+        }
+
+
+        [HttpPost]
+
+
+        public IActionResult Save([FromBody] Categoria categoria) {
+
+            categoria_services.Save(categoria);
+
+            return Ok();
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] Categoria categoria,  Guid id)
+        {
+
+            categoria_services.Update(id, categoria);
+
+            return Ok();
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+
+            categoria_services.Delete(id);
+
+            return Ok();
+
+        }
+
+
+    }
+}
---
+++ Curso_Api_Rest/Controllers/TareaController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Curso_Api_Rest.Services;
+using webapi.Modelos;
+
+namespace Curso_Api_Rest.Controllers
+{
+    [Route ("Api/{controller}")]
+    public class TareaController: ControllerBase
+    {
+        ITareaServices services;
+        public TareaController(ITareaServices tareaServices) {
+
+
+            services = tareaServices;
+
+        
+        
+        }
+
+
+        [HttpGet]
+
+        public IActionResult Get() {
+
+            services.Get();
+            return Ok();
+        
+        }
+        [HttpPost("{id_categoria}")]
+
+
+        public IActionResult Post([FromBody]Tarea tarea, Guid id_categoria)
+        {
+
+            services.Save(tarea, id_categoria);
+
+            return Ok();
+        }
+        [HttpPut("{id_tarea}/{id_categoria}")]
+
+
+        public IActionResult Put(Guid id_tarea, Guid id_categoria, Tarea tarea) {
+            services.Update(id_tarea,tarea, id_categoria);
+
+            return Ok();
+        
+        }
+
+        [HttpDelete("{id_tarea}")]
+
+
+
+        public IActionResult Delete(Guid id_tarea) {
+
+
+            services.Delete(id_tarea);
+
+            return Ok();
+        
+        
+        
+        
+        }
+
+
+
+
+    }
+
+
+
+
+    }
+}
